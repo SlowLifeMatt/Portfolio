38627bc326bc9a5d6b8103ed1ebf1820009c1fb2
Matthew Subin <slowlifematt@Matthews-MacBook-Pro.local>
2022-09-29 20:22:49 -0700

    added sidebar with menu, updated JS and README. Changed html format.

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+  
+    const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 50;
+    sectionId = current.getAttribute("id");
+
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
